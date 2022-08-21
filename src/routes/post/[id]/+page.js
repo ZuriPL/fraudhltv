f7890b686e4658ba4cf0a8000ad888c05e97259e
ZuriPL <michal.bak222@gmail.com>
2022-08-21 21:08:17 +0200

    1

--- src/routes/post/[id]/+page.js
+++ src/routes/post/[id]/+page.js
@@ -21,7 +21,8 @@ export function load({ params }) {
 		article: marked.parse(
 			`Degster: "When people make jokes about me I take it personally.”\n\n![Image](https://pbs.twimg.com/media/FahX7ylXwAEaqFT?format=jpg&name=large)\n\n“My friend kept saying that I get no women. He had no clue I had date plans with his mother for that very evening.”\n\nIt’s been reported that degster also has date plans with the mother of fellow CS pro, Hades.`
 		),
-		image: 'https://pbs.twimg.com/media/FahX7ylXwAEaqFT?format=jpg&name=large',
+		image:
+			'https://img-cdn.hltv.org/gallerypicture/BdnSJtKR6Eq08ok2p2BRen.png?ixlib=java-2.1.0&w=800&s=4304465bd0e09cc59e85be59abe7841f',
 		articleInfo: formatDate(new Date('August 19, 1975 23:15')),
 		writer: {
 			name: 'zuriii',
