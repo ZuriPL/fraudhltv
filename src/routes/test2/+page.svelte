cc1bca6bd73c576a56a233a0154a83dd935a3c28
ZuriPL <michal.bak222@gmail.com>
2022-08-23 13:30:31 +0200

    1

--- src/routes/test2/+page.svelte
+++ src/routes/test2/+page.svelte
@@ -1,9 +1,9 @@
 <svelte:head>
     <title>This is a test. Nothing more. Promise</title>
-    <meta name="twitter:title" content="This is a test." />
-    <meta name="twitter:site" content="@fraudhltv" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content="/800x400.png" />
+    <meta name="twitter:title" content="This is a test.">
+    <meta name="twitter:site" content="@fraudhltv">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:image" content="/800x400.png">
 </svelte:head>
 
 <h1>This is a test. Nothing more. Promise</h1>
