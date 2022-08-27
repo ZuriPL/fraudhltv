819857648d2dda3b47955c7a592d7bef314c960b
ZuriPL <michal.bak222@gmail.com>
2022-08-27 22:41:00 +0200

    1

--- src/routes/forums/+page.svelte
+++ src/routes/forums/+page.svelte
@@ -3,22 +3,4 @@
 </svelte:head>
 
 <script>
-    import supabase from '$lib/supabase'
-
-    async function main() {
-        const { data, error } = await supabase.from('Posts').insert([
-            {
-				title: 'test',
-				flag: 'WORLD',
-				created_at: new Date(),
-				article: '# hello world',
-				slug: 'test',
-				image: '/'
-            }
-        ])
-
-        console.log(data)
-        console.log(error)
-    }
-    main()
 </script>
