76ff7e9fd7d22a823ce409e9d8937e9a891d571d
zuriii <michal.bak222@gmail.com>
2022-10-03 12:02:18 +0000

    1

---
+++ src/routes/forum/post/[slug]/Comments.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let replies = [
+        {
+            text: 'Repluy 1',
+            replies: []
+        },
+        {
+            text: 'reply 2',
+            replies: [
+                {
+                    text: 'another reply',
+                    replies: []
+                }
+            ]
+        }
+    ]
+    // export let replies;
+</script>
+
+<div>
+    <div>
+        {replies[0].text}
+    </div>
+
+    {#each replies as reply}
+        <svelte:self replies={reply.replies}/>
+    {/each}
+</div>
